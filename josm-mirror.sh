7ae591bd9e9408fe9cb684f34ec29df77855bdb8
Simon Legner <Simon.Legner@gmail.com>
2015-04-07 21:02:38 +0200

    Fix JOSM mirror
    See https://josm.openstreetmap.de/ticket/6887#comment:10 for details.
    Now it is running without the "Evil revision hack".

--- josm-mirror.sh
+++ josm-mirror.sh
@@ -1,9 +1,25 @@
 #!/bin/bash -e
 
+date -Is
+
+cd /home/simon/mirrors/josm/
+pwd
+
+# Pull changes from Subversion
+echo $ git checkout master
+git checkout master
+echo $ git svn fetch
+git svn fetch
+echo $ git svn rebase
+git svn rebase
+
 # Changes already pulled, merge them to the mirror branch
+echo $ git branch mirror
 git branch mirror || :
+echo $ git checkout mirror
 git checkout mirror
-git merge master
+echo '$ git merge master -m"Merge branch 'master' into mirror"'
+git merge master -m"Merge branch 'master' into mirror"
 
 # Just do a plain copy of the externals into this repository.
 svn export --force http://svn.apache.org/repos/asf/ant/core/trunk/src/main/org/apache/tools/bzip2                    src/org/apache/tools/bzip2   >/dev/null
@@ -15,16 +31,21 @@ svn export --force http://svn.apache.org/repos/asf/commons/proper/codec/trunk/sr
 git config user.name "JOSM GitHub mirror"
 git config user.email "openstreetmap@v.nix.is"
 
+echo $ git add .
 git add .
+echo '$ git commit -m"josm-mirror: bumped externals"'
 git commit -m"josm-mirror: bumped externals" || :
 
-# Evil revision hack
-perl -pi -e 's[<arg value="."/>][<arg value="http://josm.openstreetmap.de/svn/trunk"/>]g' build.xml
-git commit -m"josm-mirror: evil build.xml revision hack" build.xml || :
 
 # Push the mirror to GitHub
-git remote add mirror git@github.com:openstreetmap/josm.git || :
+echo $ git remote add mirror git@github.com-josm:openstreetmap/josm.git
+git remote add mirror git@github.com-josm:openstreetmap/josm.git || :
 
 # Push to our mirrors
+echo $ git push -v mirror master
 git push -v mirror master
+echo $ git push -v mirror mirror
 git push -v mirror mirror
+
+echo Done ...
+echo
---
+++ josm-plugins-mirror.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+date -Is
+
+cd /home/simon/mirrors/josm-plugins/
+pwd
+
+# Pull changes from Subversion
+echo $ git checkout master
+git checkout master
+echo $ git svn fetch
+git svn fetch
+echo $ git svn rebase
+git svn rebase
+
+# Commit externals changes, if any
+git config user.name "JOSM GitHub mirror"
+git config user.email "openstreetmap@v.nix.is"
+
+# Push the mirror to GitHub
+echo $ git remote add mirror git@github.com-josm-plugins:openstreetmap/josm-plugins.git
+git remote add mirror git@github.com-josm-plugins:openstreetmap/josm-plugins.git || :
+echo $ git push -v mirror master
+git push -v mirror master
+
+echo Done ...
+echo
