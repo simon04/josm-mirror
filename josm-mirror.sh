868574e6588f26816827ed3ff1a4f498463c15c7
Simon Legner <Simon.Legner@gmail.com>
2020-03-25 18:42:01 +0100

    JOSM: only mirror git-svn to master
    No longer updates the mirror branch.

--- josm-mirror.sh
+++ josm-mirror.sh
@@ -14,36 +14,6 @@ git svn fetch
 echo $ git svn rebase
 git svn rebase
 
-# Changes already pulled, merge them to the mirror branch
-echo $ git branch mirror
-git branch mirror || :
-echo $ git checkout mirror
-git checkout mirror
-echo '$ git merge master -m"Merge branch 'master' into mirror"'
-git merge master -m"Merge branch 'master' into mirror"
-
-# Just do a plain copy of the externals into this repository.
-svn_external () {
-  dir_to=${1}${3:-}
-  rm -rf $dir_to
-  echo $ svn export $2 $dir_to
-  svn export --force $2 $dir_to
-}
-# To update this list, $ git svn show-externals | grep '^/' | sed 's/^./svn_external /'
-svn_external src/org/apache/commons/ https://svn.apache.org/repos/asf/commons/proper/jcs/trunk/commons-jcs-core/src/main/java/org/apache/commons/jcs jcs
-svn_external src/org/apache/commons/ https://svn.apache.org/repos/asf/commons/proper/logging/trunk/src/main/java/org/apache/commons/logging logging
-svn_external src/org/apache/commons/ https://github.com/apache/commons-compress/trunk/src/main/java/org/apache/commons/compress compress
-svn_external src/org/openstreetmap/gui https://svn.openstreetmap.org/applications/viewer/jmapviewer/src/org/openstreetmap/gui
-
-# Commit externals changes, if any
-git config user.name "JOSM GitHub mirror"
-git config user.email "openstreetmap@v.nix.is"
-
-echo $ git add .
-git add .
-echo '$ git commit -m"josm-mirror: bumped externals"'
-git commit -m"josm-mirror: bumped externals" || :
-
 # Push the mirror to GitHub
 echo $ git remote add mirror git@github.com-josm:openstreetmap/josm.git
 git remote add mirror git@github.com-josm:openstreetmap/josm.git || :
@@ -51,8 +21,6 @@ git remote add mirror git@github.com-josm:openstreetmap/josm.git || :
 # Push to our mirrors
 echo $ git push -v mirror master
 git push -v mirror master
-echo $ git push -v mirror mirror
-git push -v mirror mirror
 
 echo Done ...
 echo
